c89cf323a063e22d9f4f84ed976e89095b1eaad9
klafbang <michael.westergaard@ciber.com>
2014-08-28 16:40:47 +0200

    Front page redesign

--- src/main/java/no/ciber/academy/web/controller/RootController.java
+++ src/main/java/no/ciber/academy/web/controller/RootController.java
@@ -1,17 +1,22 @@
 package no.ciber.academy.web.controller;
 
 import no.ciber.academy.model.User;
+import no.ciber.academy.model.repository.BookInfoRepository;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class RootController {
+    @Autowired
+    private BookInfoRepository bookInfoRepository;
 
     @RequestMapping("/")
     public String index(Model model) {
     	model.addAttribute("user", new User());
+    	model.addAttribute("lostOfBooks", bookInfoRepository.findAll());
         return "index";
     }
 }
