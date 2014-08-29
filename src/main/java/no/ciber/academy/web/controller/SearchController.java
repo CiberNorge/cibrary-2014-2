843ba10f9dceca7fe08565cee19e6fae7f0b3371
Aleks Gisvold <aleksander.gisvold@ciber.com>
2014-08-29 10:43:59 +0200

    searching for stop words will now return an error message instead of crashing

--- src/main/java/no/ciber/academy/web/controller/SearchController.java
+++ src/main/java/no/ciber/academy/web/controller/SearchController.java
@@ -2,9 +2,11 @@ package no.ciber.academy.web.controller;
 
 import no.ciber.academy.model.repository.BookInfoRepository;
 import no.ciber.academy.service.SearchService;
+import org.hibernate.search.exception.EmptyQueryException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -20,10 +22,14 @@ public class SearchController {
     @Autowired
     private SearchService searchService;
 
-    //TODO consider if the searchParameter is ISBN or title
+
     @RequestMapping("/search")
-    public String search(Model bookListSearchModel, @RequestParam String searchParameter){
-        bookListSearchModel.addAttribute("listOfBooks", searchService.search(searchParameter));
+    public String search(Model bookListSearchModel, @RequestParam String searchParameter, ModelMap model){
+        try {
+            bookListSearchModel.addAttribute("listOfBooks", searchService.search(searchParameter));
+        }catch (EmptyQueryException e){
+            model.addAttribute("error", "Queries with stop words will not return results. Try searching for a full title or author name.");
+        }
         return "booklist";
     }
 
